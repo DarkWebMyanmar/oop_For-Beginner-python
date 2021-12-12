51e4102f278f7f59f0f6a96b08df337b384b5d05
KoDarkWeb <61660857+DarkWebMyanmar@users.noreply.github.com>
2021-12-12 15:46:45 +0630

    Add files via upload

---
+++ oop1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#programmer kodarkweb
+
+
+#Oop Python
+
+class Main():
+ name = "KoDarkWeb"
+ 
+ def My_fun(self):
+  age = 30
+  print(age)
+  
+  #Magic Method
+ def __init__(self):
+  print("DarkWebMyanmar")
+  
+st1 = Main()
+print(st1.name)
+st1.My_fun()
